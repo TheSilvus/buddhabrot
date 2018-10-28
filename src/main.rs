9d9d7b233ddd2a38ec8abc596f5185a9641a38d3
thesilvus <renkenor@gmail.com>
2018-10-28 20:54:23 +0100

    Added improved multi image support

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ impl math::CalculateNext for CalculateNext {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 struct Config<'a> {
     function: CalculateNext,
     initial_z: Complex64,
@@ -43,9 +43,11 @@ struct Config<'a> {
 
     scan_min: Complex64,
     scan_max: Complex64,
-    iterations: usize,
     samples: usize,
     sample_section: usize,
+
+    check_iterations: usize,
+    images: Vec<ImageConfig<'a>>,
 
     eta_section: usize,
     eta_time: u64,
@@ -57,13 +59,20 @@ struct Config<'a> {
     file_buffer_size: usize,
     pixel_buffer_cutoff_size: usize,
 
-    mbh_width: u64,
-    mbh_height: u64,
-    mbh_min: Complex64,
-    mbh_max: Complex64,
-    mbh_file_name: &'a str,
-
     image_file_name: &'a str,
+}
+#[derive(Clone)]
+struct ImageConfig<'a> {
+    min_iterations: usize,
+    max_iterations: usize,
+
+    width: u64,
+    height: u64,
+
+    min: Complex64,
+    max: Complex64,
+
+    file_name: &'a str,
 }
 
 fn main() {
@@ -78,51 +87,81 @@ fn main() {
 
         scan_min: Complex64::new(-2.0, -2.0),
         scan_max: Complex64::new(2.0, 2.0),
-        iterations: 10_000,
-        samples: 1e7 as usize,
+        samples: 1e8 as usize,
         sample_section: 1e6 as usize,
+
+        check_iterations: 2000,
+        images: vec![
+            ImageConfig {
+                min_iterations: 0,
+                max_iterations: 100,
+
+                width: 1000,
+                height: 1000,
+
+                min: Complex64::new(-2.0, -2.0),
+                max: Complex64::new(2.0, 2.0),
+
+                file_name: "image-1.mbh",
+            },
+            ImageConfig {
+                min_iterations: 50,
+                max_iterations: 500,
+
+                width: 1000,
+                height: 1000,
+
+                min: Complex64::new(-2.0, -2.0),
+                max: Complex64::new(2.0, 2.0),
+
+                file_name: "image-2.mbh",
+            },
+            ImageConfig {
+                min_iterations: 200,
+                max_iterations: 2000,
+
+                width: 1000,
+                height: 1000,
+
+                min: Complex64::new(-2.0, -2.0),
+                max: Complex64::new(2.0, 2.0),
+
+                file_name: "image-3.mbh",
+            },
+        ],
 
         eta_section: 10,
         eta_time: 1000,
 
-        threads: 12,
+        threads: 4,
         channel_buffer: 50,
         thread_buffer: 1_000_000,
 
         file_buffer_size: 1e6 as usize,
         pixel_buffer_cutoff_size: 1e6 as usize,
 
-        mbh_width: 1000,
-        mbh_height: 1000,
-        mbh_min: Complex64::new(-2.0, -2.0),
-        mbh_max: Complex64::new(2.0, 2.0),
-        mbh_file_name: "image.mbh",
-
         image_file_name: "image.png",
     };
 
-    generate(config);
-    image(config);
+    generate(config.clone());
+    image(config.clone());
 }
 
 fn generate(config: Config<'static>) {
     println!(
         "Estimated maximum RAM usage: {}mb",
         ((config.threads + config.channel_buffer) * config.thread_buffer * 2 * 8
-            + (config.mbh_width as usize * config.mbh_height as usize / config.file_buffer_size
+            + (config
+                .images
+                .iter()
+                .map(|image| image.width as usize * image.height as usize)
+                .sum::<usize>()
+                / config.file_buffer_size
                 + 1)
                 * config.pixel_buffer_cutoff_size)
             / 1000000
     );
-    println!(
-        "Estimated typical maximum RAM usage: {}mb",
-        (config.threads * config.thread_buffer * 2 * 8
-            + (config.channel_buffer / 2) * config.thread_buffer * 2 * 8
-            + (config.mbh_width as usize * config.mbh_height as usize / config.file_buffer_size
-                + 1)
-                * (config.pixel_buffer_cutoff_size / 2))
-            / 1000000
-    );
+
     let location_generator = location_generators::UniformRandomLocationGenerator::new(
         config.scan_min,
         config.scan_max,
@@ -131,26 +170,29 @@ fn generate(config: Config<'static>) {
     );
     let eta = eta::ETA::new(config.samples, config.eta_section, config.eta_time);
 
-    let (sender1, receiver1) = mpsc::sync_channel::<Option<Vec<Complex64>>>(config.channel_buffer);
-    let (sender2, receiver2) = mpsc::sync_channel::<Option<Vec<Complex64>>>(config.channel_buffer);
-    let (sender3, receiver3) = mpsc::sync_channel::<Option<Vec<Complex64>>>(config.channel_buffer);
+    let mut senders = vec![];
+    let mut receivers = vec![];
+    for _ in &config.images {
+        let (sender, receiver) =
+            mpsc::sync_channel::<Option<Vec<Complex64>>>(config.channel_buffer);
+        senders.push(sender);
+        receivers.push(receiver);
+    }
 
     for thread_id in 0..config.threads {
         let mut location_generator = location_generator.clone();
         let mut eta = eta.clone();
 
-        let sender1 = sender1.clone();
-        let sender2 = sender2.clone();
-        let sender3 = sender3.clone();
+        let senders = senders.clone();
+        let config = config.clone();
 
         thread::Builder::new()
             .name(format!("Calculator {}", thread_id))
             .spawn(move || {
                 println!("Starting thread {}", thread_id);
 
-                let mut result_cache1 = Vec::with_capacity(config.thread_buffer);
-                let mut result_cache2 = Vec::with_capacity(config.thread_buffer);
-                let mut result_cache3 = Vec::with_capacity(config.thread_buffer);
+                let mut result_caches =
+                    vec![Some(Vec::with_capacity(config.thread_buffer)); config.images.len()];
 
                 while let Some(c) = location_generator.next_location() {
                     eta.count();
@@ -164,73 +206,77 @@ fn generate(config: Config<'static>) {
                         config.initial_z,
                         config.bailout_min,
                         config.bailout_max,
-                        config.iterations,
+                        config.check_iterations,
                     ) {
-                        if bailout < 100 {
-                            math::calculate_iteration_values(
-                                &mut config.function.get(c),
-                                config.initial_z,
-                                config.bailout_min,
-                                config.bailout_max,
-                                0,
-                                100,
-                                &mut result_cache1,
-                            );
-                        }
-                        if bailout < 1000 {
-                            math::calculate_iteration_values(
-                                &mut config.function.get(c),
-                                config.initial_z,
-                                config.bailout_min,
-                                config.bailout_max,
-                                0,
-                                1000,
-                                &mut result_cache2,
-                            );
-                        }
-                        if bailout < 10_000 {
-                            math::calculate_iteration_values(
-                                &mut config.function.get(c),
-                                config.initial_z,
-                                config.bailout_min,
-                                config.bailout_max,
-                                0,
-                                10_000,
-                                &mut result_cache3,
-                            );
+                        for (i, image) in config.images.iter().enumerate() {
+                            if image.min_iterations <= bailout && bailout < image.max_iterations {
+                                math::calculate_iteration_values(
+                                    &mut config.function.get(c),
+                                    config.initial_z,
+                                    config.bailout_min,
+                                    config.bailout_max,
+                                    image.min_iterations,
+                                    image.max_iterations,
+                                    &mut result_caches[i].as_mut().unwrap(),
+                                );
+                            }
                         }
                     }
 
-                    if result_cache1.len() > config.thread_buffer {
-                        send_with_warning(&sender1, Some(result_cache1));
-                        result_cache1 = Vec::with_capacity(config.thread_buffer);
-                    }
-                    if result_cache2.len() > config.thread_buffer {
-                        send_with_warning(&sender2, Some(result_cache2));
-                        result_cache2 = Vec::with_capacity(config.thread_buffer);
-                    }
-                    if result_cache3.len() > config.thread_buffer {
-                        send_with_warning(&sender3, Some(result_cache3));
-                        result_cache3 = Vec::with_capacity(config.thread_buffer);
+                    for (i, _) in config.images.iter().enumerate() {
+                        if result_caches[i].as_ref().unwrap().len() > config.thread_buffer {
+                            send_with_warning(&senders[i], Some(result_caches[i].take().unwrap()));
+                            result_caches[i] = Some(Vec::with_capacity(config.thread_buffer));
+                        }
                     }
                 }
-                send_with_warning(&sender1, Some(result_cache1));
-                send_with_warning(&sender1, None);
-                send_with_warning(&sender2, Some(result_cache2));
-                send_with_warning(&sender2, None);
-                send_with_warning(&sender3, Some(result_cache3));
-                send_with_warning(&sender3, None);
+
+                for (i, _) in config.images.iter().enumerate() {
+                    send_with_warning(&senders[i], Some(result_caches[i].take().unwrap()));
+                    send_with_warning(&senders[i], None);
+                }
 
                 println!("Thread {} done", thread_id);
             }).expect("Unable to start thread");
     }
 
-    let handle1 = run_aggregator(config, receiver1, Some("image-1.mbh"));
-    let handle2 = run_aggregator(config, receiver2, Some("image-2.mbh"));
-    let handle3 = run_aggregator(config, receiver3, Some("image-3.mbh"));
-    handle1.join().unwrap();
-    handle2.join().unwrap();
-    handle3.join().unwrap();
+    let mut handles = Vec::<thread::JoinHandle<()>>::new();
+    for (i, receiver) in receivers.drain(..).enumerate() {
+        let config = config.clone();
+        let image = config.images[i].clone();
+
+        handles.push(
+            thread::Builder::new()
+                .name("Aggregator".to_owned())
+                .spawn(move || {
+                    let mut aggregator = aggregators::FileAggregator::create(
+                        image.file_name,
+                        image.width,
+                        image.height,
+                        image.min,
+                        image.max,
+                        config.file_buffer_size,
+                        config.pixel_buffer_cutoff_size,
+                    ).expect("Error while setting up aggregator");
+
+                    let mut received = 0;
+                    while received < config.threads {
+                        let result = receiver.recv().unwrap();
+                        if let Some(result) = result {
+                            for c in result {
+                                aggregator.aggregate(c);
+                            }
+                        } else {
+                            received += 1;
+                        }
+                    }
+                }).expect("Unable to start thread"),
+        );
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
 fn send_with_warning(
     sender: &mpsc::SyncSender<Option<Vec<Complex64>>>,
@@ -245,75 +291,43 @@ fn send_with_warning(
         _ => panic!(),
     }
 }
-fn run_aggregator(
-    config: Config<'static>,
-    receiver: mpsc::Receiver<Option<Vec<Complex64>>>,
-    file_override: Option<&'static str>,
-) -> thread::JoinHandle<()> {
-    thread::Builder::new()
-        .name("Aggregator".to_owned())
-        .spawn(move || {
-            let mut aggregator = aggregators::FileAggregator::create(
-                file_override.unwrap_or(config.mbh_file_name),
-                config.mbh_width,
-                config.mbh_height,
-                config.mbh_min,
-                config.mbh_max,
-                config.file_buffer_size,
-                config.pixel_buffer_cutoff_size,
-            ).expect("Error while setting up aggregator");
-
-            let mut received = 0;
-            while received < config.threads {
-                let result = receiver.recv().unwrap();
-                if let Some(result) = result {
-                    for c in result {
-                        aggregator.aggregate(c);
-                    }
-                } else {
-                    received += 1;
-                }
-            }
-        }).expect("Unable to start thread")
-}
 
 fn image(config: Config) {
     // TODO separate image size; downsampling
     println!("Preparing color channels");
 
-    let file_names = vec!["image-1.mbh", "image-2.mbh", "image-3.mbh"];
-    let images = file_names
-        .iter()
-        .map(|file_name| {
-            println!("Loading image from {}", file_name);
-            let mut file = OpenOptions::new()
-                .read(true)
-                .open(file_name)
-                .expect("Could not open file");
+    for image in config.images {
+        println!("Loading image from {}", image.file_name);
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(image.file_name)
+            .expect("Could not open file");
 
-            use std::f64::consts::E;
-            use num;
+        use num;
+        use std::f64::consts::E;
 
-            image::ImageData::read_fully(
+        image::ImageData::read_fully(
                 &mut file,
-                config.mbh_width as usize,
-                config.mbh_height as usize,
+                image.width as usize,
+                image.height as usize,
             ).expect("Could not read file")
             // .map(&|i: u32| ((i as f64).sqrt() * 10000.0) as u32)
             // .map_to_grayscale_linear(1.0)
             .map_to_image1(&|i, highest| num::clamp((1.0 - E.powf(-2.0 * (i as f64 / highest as f64))) * 255.0 * 2.0, 0.0, 255.0) as u8, file_image::Gray(8))
-        }).collect::<Vec<image::Image>>();
+            .save(&(image.file_name.to_owned() + ".png"))
+            .unwrap();
+    }
 
     // TODO tiled writing?
 
-    println!("Joining images");
-    let image = image::Image::join(
-        &images[2],
-        &images[1],
-        &images[0],
-        file_image::ColorType::RGB(8),
-    );
+    // println!("Joining images");
+    // let image = image::Image::join(
+    //     &images[2],
+    //     &images[1],
+    //     &images[0],
+    //     file_image::ColorType::RGB(8),
+    // );
 
-    println!("Saving image");
-    image.save(config.image_file_name).unwrap();
+    // println!("Saving image");
+    // image.save(config.image_file_name).unwrap();
 }
